db447c846db79b0ee786ed85b08fe806a22edcf2
Joshua Miron <josh.miron@allin.com>
2023-02-12 15:17:55 -0500

    Movie Details page components.

---
+++ src/components/movieDetails/index.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import MonetizationIcon from "@mui/icons-material/MonetizationOn";
+import StarRate from "@mui/icons-material/StarRate";
+import Typography from "@mui/material/Typography";
+
+const styles = {
+    root: {
+        chipSet: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        listStyle: "none",
+        padding: 1.5,
+        margin: 0,
+        },
+        chipLabel: {
+            margin: 0.5,
+        },
+    },
+    //Was there a curly brace and comma missing from the tutorial copy-code block?  
+    //I was getting an error until I added the above "}," to close "root".
+};
+
+const MovieDetails = ( props) => {
+  const movie = props.movie
+
+  return (
+    <>
+      <Typography variant="h5" component="h3">
+        Overview
+      </Typography>
+
+      <Typography variant="h6" component="p">
+        {movie.overview}
+      </Typography>
+
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip label="Genres" sx={styles.chipLabel} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name}  />
+          </li>
+        ))}
+      </Paper>
+      <Paper component="ul" sx={styles.chipSet}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count}`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+      </>
+  );
+};
+export default  MovieDetails ;
