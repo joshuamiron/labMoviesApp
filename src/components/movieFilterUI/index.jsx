c0558fa91b48df9a8162545e35bddc632978630f
Joshua Miron <josh.miron@allin.com>
2023-03-05 14:30:57 -0500

    Added Upcoming movies page.

---
+++ src/components/movieFilterUI/index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import FilterCard from "../filterMoviesCard";
+import Fab from "@mui/material/Fab";
+import Drawer from "@mui/material/Drawer";
+
+export const titleFilter = function (movie, value) {
+  return movie.title.toLowerCase().search(value.toLowerCase()) !== -1;
+};
+
+export const genreFilter = function (movie, value) {
+  const genreId = Number(value);
+  return genreId > 0 ? movie.genre_ids.includes(genreId) : true;
+};
+
+const styles = {
+  root: {
+    backgroundColor: "#bfbfbf",
+  },
+  fab: {
+    marginTop: 8,
+    position: "fixed",
+    top: 20,
+    right: 2,
+  },
+};
+
+const MovieFilterUI = ({ onFilterValuesChange, titleFilter, genreFilter }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  return (
+    <>
+      <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        sx={styles.fab}
+      >
+        Filter
+      </Fab>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <FilterCard
+          onUserInput={onFilterValuesChange}
+          titleFilter={titleFilter}
+          genreFilter={genreFilter}
+        />
+      </Drawer>
+    </>
+  );
+};
+
+export default MovieFilterUI;
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import {getMovies} from "../api/tmdb-api";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
-  const favourites = movies.filter(m => m.favourite)
+  const favourites = movies.filter((m) => m.favourite)
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [],
+    [titleFiltering, genreFiltering]
+  );
+
   localStorage.setItem('favourites', JSON.stringify(favourites))
 
   const addToFavourites = (movieId) => {
@@ -14,19 +35,38 @@ const HomePage = (props) => {
     setMovies(updatedMovies);
   };
 
+  const changeFilterValues = (type, value) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
   useEffect(() => {
-    getMovies().then(movies => {
+    getMovies().then((movies) => {
       setMovies(movies);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const displayedMovies = filterFunction(movies);
+
   return (
-    <PageTemplate
-      title='Discover Movies'
-      movies={movies}
-      selectFavourite={addToFavourites}
-    />
+    <>
+      <PageTemplate
+        title='Discover Movies'
+        movies={displayedMovies}
+        selectFavourite={addToFavourites}
+      />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
+    
   );
 };
 export default HomePage;
