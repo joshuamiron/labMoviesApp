b0ab01e8a490862e53cc1e36e68820a79bef3555
Joshua Miron <josh.miron@allin.com>
2023-04-11 09:30:31 -0400

    Adjusting Site Header to be dropdown menus.

--- src/components/siteHeader/index.jsx
+++ src/components/siteHeader/index.jsx
@@ -11,6 +11,7 @@ import MenuItem from "@mui/material/MenuItem";
 import { useNavigate } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Grid from "@mui/material/Grid";
 
 const styles = {
   title: {
@@ -27,11 +28,13 @@ const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 const SiteHeader = () => {
   const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = useState(null);
+
   const open = Boolean(anchorEl);
+
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
 
-  const menuOptions = [
+  /* const menuOptions = [
     { label: "Home", path: "/" },
     { label: "Upcoming", path: "/movies/upcoming"},
     { label: "Favorites", path: "/movies/favourites" },
@@ -39,6 +42,36 @@ const SiteHeader = () => {
     { label: "Popular Movies", path: "/movies/popular" },
     { label: "Made Up Movies", path: "/movies/mymadeupmoviespage" },
     { label: "Trending Movies", path: "/movies/trending" },
+    { label: "Trending People", path: "/people/trending" },
+  ]; */
+
+  const mobileMenuOptions = [
+    { label: "Home", path: "/" },
+    { label: "Upcoming", path: "/movies/upcoming"},
+    { label: "Favorites", path: "/movies/favourites" },
+    { label: "My Playlist", path: "/movies/myplaylist" },
+    { label: "Popular Movies", path: "/movies/popular" },
+    { label: "Made Up Movies", path: "/movies/mymadeupmoviespage" },
+    { label: "Trending Movies", path: "/movies/trending" },
+    { label: "Trending People", path: "/people/trending" },
+  ]; 
+
+  const movieMenuOptions = [
+    { label: "Home", path: "/" },
+    { label: "Upcoming", path: "/movies/upcoming"},
+    { label: "Favorites", path: "/movies/favourites" },
+    { label: "My Playlist", path: "/movies/myplaylist" },
+    { label: "Popular Movies", path: "/movies/popular" },
+    { label: "Made Up Movies", path: "/movies/mymadeupmoviespage" },
+    { label: "Trending Movies", path: "/movies/trending" },
+  ];
+
+  const tvMenuOptions = [
+    { label: "Home", path: "/" },
+  ];
+
+  const peopleMenuOptions = [
+    { label: "Home", path: "/" },
     { label: "Trending People", path: "/people/trending" },
   ];
 
@@ -48,6 +81,13 @@ const SiteHeader = () => {
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
@@ -87,7 +127,7 @@ const SiteHeader = () => {
                 open={open}
                 onClose={() => setAnchorEl(null)}
               >
-                {menuOptions.map((opt) => (
+                {mobileMenuOptions.map((opt) => (
                   <MenuItem
                     key={opt.label}
                     onClick={() => handleMenuSelect(opt.path)}
@@ -99,7 +139,7 @@ const SiteHeader = () => {
             </>
           ) : (
             <>
-              {menuOptions.map((opt) => (
+              {/*{menuOptions.map((opt) => (
                 <Button
                   key={opt.label}
                   color="inherit"
@@ -107,7 +147,43 @@ const SiteHeader = () => {
                 >
                   {opt.label}
                 </Button>
-              ))}
+              ))}*/}
+              <Grid>
+                <Button
+                  onClick={handleClick} color="inherit">
+                    Movies
+                </Button>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  {movieMenuOptions.map((opt) => (
+                    <MenuItem 
+                      key={opt.label}
+                      onClick={() => handleMenuSelect(opt.path)}>
+                        {opt.label}
+                    </MenuItem>
+                  ))}
+                </Menu>
+                <Button
+                  onClick={handleClick} color="inherit">
+                    People
+                </Button>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  {peopleMenuOptions.map((opt) => (
+                    <MenuItem 
+                      key={opt.label}
+                      onClick={() => handleMenuSelect(opt.path)}>
+                        {opt.label}
+                    </MenuItem>
+                  ))}
+                </Menu>         
+              </Grid>
             </>
           )}
         </Toolbar>
