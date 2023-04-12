0d44e91661e4bebfa8bca287603a000210dcc809
Joshua Miron <josh.miron@allin.com>
2023-04-11 23:53:44 -0400

    Attempting to deploy to Vercel, but its choking on ..components/movieReview.  GitHub has the folder named as MovieReview for some reason. Deleted and recreated movieReview.

--- src/components/MovieReview/index.jsx
+++ src/components/MovieReview/index.jsx
@@ -5,7 +5,7 @@ const MovieReview =  ({ review }) => {
   return (
   <>
     <Typography variant="h5" component="h3">
-      Review By: {review.author}
+      Review author: {review.author}
     </Typography>
 
     <Typography variant="h6" component="p">
