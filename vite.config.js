4c935ba2f4b4ebe80325762617593d385d49e0c7
Joshua W Miron <jmiron@gmail.com>
2023-05-07 11:52:03 -0400

    Revert "Creating a new version of Movies app for Express API assignment."
    This reverts commit 1cb26ce844e3053e1f3ecc6e5ca4e1ed369101eb.

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
 })
